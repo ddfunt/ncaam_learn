05476f8cb7c9aa8d4aabc5bc2cadec7f5ceee36b
Matt <diracdeltafunct@gmail.com>
2019-03-06 22:24:55 -0500

    added some parsing

--- orm.py
+++ orm.py
@@ -3,4 +3,6 @@
 class Team:
 
     name = None
-    
+    _url = None
+    year = None
+    games = None
--- pom_scrape.py
+++ pom_scrape.py
@@ -1,4 +1,7 @@
 import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
 
 base = 'https://kenpom.com/'
 login_suffix = 'handlers/login_handler.php'
@@ -8,9 +11,39 @@ password = 'uvawahoowa'
 
 
 payload = {'email': user, 'password': password}
-sess = requests.Session()
 
-sess.post(base+login_suffix, payload)
+def open_session():
+    sess = requests.Session()
 
-d = sess.get('https://kenpom.com/team.php?team=Gonzaga')
-print(d.text)
+    sess.post(base+login_suffix, payload)
+    return sess
+
+def get_page(url, session):
+    resp = session.get(url).text
+
+    return BeautifulSoup(resp, 'lxml')
+
+
+
+
+if __name__ == '__main__':
+    sess = open_session()
+
+    home = get_page(base, sess)
+    years_search = home.find_all('div', id='years-container')
+    years_raw = years_search[0].find_all('a')
+    years_links = {year.text: year['href'] for year in years_raw}
+
+    page = get_page(base + years_links['2018'], sess)
+    data_table = page.find_all('table', id='ratings-table')[0]
+    body = data_table.find_all('tbody')[0]
+    rows = body.find_all('tr')
+
+    for row in rows:
+        d = row.find_all('td')
+        print(d)
+
+
+
+    #table = pd.read_html(str(data_table[0]))[0]
+
