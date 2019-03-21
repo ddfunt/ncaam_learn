aed9424b01296b28737f2a8c7fd7b313c50be2a9
Matt <diracdeltafunct@gmail.com>
2019-03-20 21:01:44 -0400

    added random coinflip result

--- predict2019.py
+++ predict2019.py
@@ -11,7 +11,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import numpy as np
 from keras_learn import load_data
-
+import random
+import numpy as np
 
 def construct_game(team1, team2):
     d = [1]
@@ -22,16 +23,18 @@ def construct_game(team1, team2):
 
 model = create_model()
 compile_model(model)
-checkpoint_path = "training_best/cp.ckpt"
+checkpoint_path = "training_1/cp.ckpt"
 
 
-def load_bracket(file='finals.csv'):
+def load_bracket(file='bracket.csv'):
     d = []
     with open(file, 'r') as f:
         for row in f:
             d.append(row.strip().split(','))
     return d
 
+def flip(p):
+    return 1 if random.random() < p else 0
 
 if __name__ == '__main__':
     bracket = load_bracket()
@@ -41,23 +44,12 @@ if __name__ == '__main__':
     output = []
     for g in bracket:
         if g[1]:
-            print(list(teams.keys()))
             team1 = teams[f'{g[0].strip()}_2019']
             team2 = teams[f'{g[1].strip()}_2019'] #Maryland Eastern Shore
-            #game = construct_game(team1, team2)
-            #print(game)
-
-            #loss, acc = model.evaluate(game, np.array([1]))
-            #print("Untrained model, accuracy: {:5.2f}%".format(100 * acc))
-
+            team_names = [team1.name, team2.name]
             model.load_weights(checkpoint_path)
-            #loss, acc = model.evaluate(game, np.array([1]))
-            #print("Restored model, accuracy: {:5.2f}%".format(100 * acc))
-            #print(loss)
-
 
             game = construct_game(team1, team2)
-            print(np.shape(game))
             predictions = model.predict(game)[0]
             print(f'{team1.name} win:  {predictions[1]*100:.2f}%')
             print(f'{team1.name} loss: {predictions[0]*100:.2f}%')
@@ -68,11 +60,16 @@ if __name__ == '__main__':
             print('')
             print(f'Unknown Diff: {(abs(predictions[1]-predictions_invert[0])*100):.4f}')
 
-            output.append(f'{g[0]}, {g[1]}, {predictions[1]*100:.2f},{predictions[0]*100:.2f},{predictions_invert[1]*100:.2f},{predictions_invert[0]*100:.2f}\n')
+            team1_win = np.average([predictions[1], predictions_invert[0]])
+            team2_win = np.average([predictions[0], predictions_invert[1]])
+            print(team1_win, team2_win)
+            result = flip(team2_win)
+            print(result)
+            output.append(f'{g[0]}, {g[1]}, {predictions[1]*100:.2f},{predictions[0]*100:.2f},{predictions_invert[1]*100:.2f},{predictions_invert[0]*100:.2f}, {team_names[result]}\n')
 
         else:
             output.append(f'{g[0]}, {g[1]},-,-,-,-\n')
 
-    with open('fit_finals.csv', 'w') as f:
+    with open('fit_64.csv', 'w') as f:
         for row in output:
             f.write(row)
